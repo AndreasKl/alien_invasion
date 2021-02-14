29e803ec9ffc2a33325944d5ddb7d7af34a0bdd3
Andreas Kluth <mail@andreaskluth.net>
2021-02-14 23:07:45 +0100

    Minor cleanups.

--- prototypes/particles/particles.py
+++ prototypes/particles/particles.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pygame, sys, random
 
 clock = pygame.time.Clock()
@@ -9,7 +11,7 @@ class Particles:
 
         self.running = True
         self.screen = pygame.display.set_mode((1024, 1024))
-        self.particles = []
+        self.typed_particles: List[CircleParticle] = []
         pygame.display.set_caption("Particles everywhere")
 
     def _check_events(self):
@@ -29,45 +31,58 @@ class Particles:
             pygame.display.update()
 
     def _start_particles(self):
-        self.particles.append(
-            [
-                [
-                    (1024 / 2) + 20,
-                    400
-                ],  # Start location
-                [
-                    random.randint(-20, 20) / 10,
-                    -2
-                ],  # offsets
-                random.randint(2, 6),  # Size
-                (random.randint(230, 255), random.randint(0, 150), random.randint(0, 10))  # Color
-            ]
-        )
-        self.particles.append(
-            [
-                [
-                    (1024 / 2) - 20,
-                    400
-                ],  # Start location
-                [
-                    random.randint(-20, 20) / 10,
-                    -2
-                ],  # offsets
-                random.randint(2, 6),  # Size
-                (random.randint(230, 255), random.randint(0, 150), random.randint(0, 10))  # Color
-            ]
-        )
+        self.typed_particles.append(
+            CircleParticle(
+                int((1024 / 2) + 20),
+                400,
+                random.randint(0, 20) / 10.0 - 1,
+                random.randint(0, 5) / 10.0 - 1,
+                random.randint(2, 30),
+                self.some_kind_of_red()
+            ))
+        self.typed_particles.append(
+            CircleParticle(
+                int((1024 / 2) - 20),
+                400,
+                random.randint(0, 20) / 10.0 - 1,
+                random.randint(0, 5) / 10.0 - 1,
+                random.randint(2, 30),
+                self.some_kind_of_red()
+            ))
+
         particles_to_remove = []
-        for particle in self.particles:
-            particle[0][0] += particle[1][0]
-            particle[0][1] -= particle[1][1]
-            particle[2] -= 0.1
-            color = particle[3]
-            pygame.draw.circle(self.screen, color, [int(particle[0][0]), int(particle[0][1])], particle[2])
-            if particle[2] <= 0:
+        for particle in self.typed_particles:
+            particle.move()
+            pygame.draw.circle(self.screen, particle.color, particle.get_xy(), particle.get_size())
+            if particle.get_size() <= 0:
                 particles_to_remove.append(particle)
         for to_remove in particles_to_remove:
-            self.particles.remove(to_remove)
+            self.typed_particles.remove(to_remove)
+
+    def some_kind_of_red(self):
+        return (random.randint(230, 255), random.randint(0, 150), random.randint(0, 10))
+
+
+class CircleParticle:
+
+    def __init__(self, start_x: int, start_y: int, offset_x: float, offset_y: float, size: int, color: (int, int, int)):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+        self.size = size
+        self.color = color
+
+    def move(self):
+        self.start_x += self.offset_x
+        self.start_y -= self.offset_y
+        self.size -= 0.12
+
+    def get_xy(self):
+        return [int(self.start_x), int(self.start_y)]
+
+    def get_size(self):
+        return self.size
 
 
 if __name__ == '__main__':
